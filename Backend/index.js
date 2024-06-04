a7664975167ce0a7e3c44e718e4221a3683c67b7
Mohammed Furqan <69319082+Furqan619@users.noreply.github.com>
2024-06-04 19:03:35 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(cors(
     {
-        origin: ["https://e-library-2-frontend.vercel.app"],
+        origin: ["https://e-library-2-api.vercel.app"],
         methods: ["POST", "GET"],
         credentials: true
     }
