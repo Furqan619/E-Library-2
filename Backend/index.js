8956130d8390ec4cb0de8f9f3498e4e714587b8a
Mohammed Furqan <69319082+Furqan619@users.noreply.github.com>
2024-06-06 06:51:25 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -8,24 +8,24 @@ import userRoute from "./route/user.route.js";
 
 const app = express();
 
-app.use(cors());
-//    {
-//        origin: ["http://e-library-2-client.vercel.app/"],
-//        methods: ["POST", "GET"],
-//        credentials: true
-//    }
+app.use(cors(
+    {	origin:["https://deploy-mern-1whq.vercel.app"],
+	    methods: ["POST", "GET"],
+	    credentials: true
+    }
+));
 app.use(express.json());
 
 dotenv.config();
 
-//const PORT = process.env.PORT || 4000;
-//const URI = process.env.MongoDBURI;
+const PORT = process.env.PORT || 4000;
+const URI = process.env.MongoDBURI;
 
 // connect to mongoDB
 try {
-    mongoose.connect("mongodb+srv://mdfurqan127:2422466@Atlas@cluster0.ngxbq6o.mongodb.net/books_database", {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
+    mongoose.connect(URI, {
+        // useNewUrlParser: true,
+        // useUnifiedTopology: true,
     });
     console.log("Connected to mongoDB");
 } catch (error) {
